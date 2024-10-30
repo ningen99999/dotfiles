36a3f313b8eb921ef1fe87680fe7f12b3b4a6e37
ningen99999 <ningen99999@gmail.com>
2024-10-29 21:49:48 -0400

    updated to current

---
+++ bspwm/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#xrandr -s 1920x1080
+
+#polybar &
+
+#fcitx
+
+#feh --bg-fill --geometry +0-850 ~/Pictures/dona-art.jpg
+
+#pgrep -x sxhkd > /dev/null || sxhkd &
+
+#xwinwrap -b -s -fs -st -nf -ov -fdt -- mpv -wid WID --loop --framedrop=vo --no-audio --panscan="1.0" ~/Downloads/youtube-dls/dona-asmr.webm
